fa38e44ea7c65fd530229e5c122e3f44058199a0
NPCWOW <Sentorandom@gmail.com>
2026-08-07 04:14:50 +0600

    Suppress extension hydration attributes on body

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ru">
-      <body>
+      <body suppressHydrationWarning>
         {children}
         <AmbientMotion />
       </body>
